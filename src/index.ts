f2996890dba44b5ca20bd1be24ce22e208402d7b
PrinceSachan <sachanprince096@gmail.com>
2024-08-20 22:54:05 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { Express, Application, Request, Response } from 'express'
+
+const app: Application = express()
+const port = 8000;
+
+// app.get('/', (req: Request, res: Response) => {
+//     res.send('Hello youChat')
+// });
+
+// app.listen(port, () => {
+//     console.log(`Listing from ${port}`)
+// })
